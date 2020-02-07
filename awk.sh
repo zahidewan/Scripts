41966e781e92b0008d6bd13067a4fef51212ccb1
Zahid Dewan <32001321+zahidewan@users.noreply.github.com>
2020-02-06 20:05:16 -0600

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -1,5 +1,5 @@
 #zahid Dewan
 #Script to Search through network logs using awk and find IP, timestamps, UserID and MAC Address 
+#change/modify UID strings depending on your UID format 
 
-
-egrep $1 | awk '{match($0,/[a-zA-Z][a-zA-Z][0- 9][0-9][0-9][0-9][a-zZ-Z][a-zA-Z]/) ; starID = substr($0,RSTART,RLENGTH); match($1,/[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]/) ; timestamp = substr($1,RSTART,RLENGTH) ; match($0,/[0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]\- [0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]/); macaddr = substr($0,RSTART,RLENGTH); print "Time Stamp: "timestamp " | Star ID: " starID " | Mac-Address: " macaddr}'
+egrep $1 | awk '{match($0,/[a-zA-Z][a-zA-Z][0- 9][0-9][0-9][0-9][a-zZ-Z][a-zA-Z]/) ; UID = substr($0,RSTART,RLENGTH); match($1,/[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]/) ; timestamp = substr($1,RSTART,RLENGTH) ; match($0,/[0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]\- [0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]\-[0-9a-f][0-9a-f]/); macaddr = substr($0,RSTART,RLENGTH); print "Time Stamp: "timestamp " | Star ID: " starID " | Mac-Address: " macaddr}'
